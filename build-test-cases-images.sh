989ecdf5658796a2536ce716f98ea28761ebf624
Yang Jun01 <yangjun01@4paradigm.com>
2024-07-29 07:20:28 +0000

    update

--- build-test-cases-images.sh
+++ build-test-cases-images.sh
@@ -5,10 +5,13 @@ if [ -f $model_list ]
 then
     while IFS= read -r model_name
     do
-        printf '%s\n' "$model_name"
-        image_name="${base_image}-${model_name}"
-        echo "$image_name"
-        docker build --build-arg MODEL_NAME=${model_name} -t ${image_name} -f build-test-cases-image.dockerfile .
+	if [ ! -z "$model_name" ]
+	then
+	    printf '%s\n' "$model_name"
+            image_name="${base_image}-${model_name}"
+            echo "$image_name"
+            docker build --build-arg MODEL_NAME=${model_name} -t ${image_name} -f build-test-cases-image.dockerfile .
+	fi
     done < "$model_list"
 else
     echo "model.list not exists"
